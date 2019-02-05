2ea952134890d3d1602f535810aba7c632e2e15f
FERNANDO TEIXEIRA SILVA <40283818+FernandoT3ix3ir4@users.noreply.github.com>
2019-02-05 01:09:39 -0200

    Create
    create

---
+++ embaralhator2000/BO.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace embaralhator2000
+{
+    public static class BO
+    {
+        public static List<string> GeradorDeMassa(int qtde)
+        {
+            var retorno = new List<string>();
+
+            try
+            {
+                for (int i = 0; i < qtde; i++)
+                {
+                    var numero = DateTime.Now.Millisecond + i;
+
+                    numero = numero * numero;
+
+                    string x = string.Format("Item:{0};{1}", (i + 1), RetornaLetra(numero));
+
+                    retorno.Add(x);
+                }
+
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+            return retorno;
+        }
+
+        private static string RetornaLetra(int hash)
+        {
+            var retorno = string.Empty;
+            int resultado = 0;
+
+
+            if (hash != int.MinValue)
+                retorno = CalculaHashERetornaLetra(hash, ref resultado);
+
+            return retorno;
+        }
+
+        private static string CalculaHashERetornaLetra(int hash, ref int resultado)
+        {
+            string retorno;
+            {
+                var valor = hash.ToString().Replace("-", "");
+
+                resultado = CalculaHash(resultado, valor);
+
+                if (resultado <= 2)
+                    retorno = "A";
+                else if (resultado <= 4)
+                    retorno = "B";
+                else if (resultado <= 6)
+                    retorno = "C";
+                else if (resultado <= 8)
+                    retorno = "D";
+                else
+                    retorno = "E";
+            }
+
+            return retorno;
+        }
+
+        private static int CalculaHash(int resultado, string valor)
+        {
+            resultado = RetornaNumeroUnitario(valor, resultado);
+
+            while (resultado.ToString().Length > 1)
+            {
+                string novoValor = resultado.ToString();
+
+                resultado = RetornaNumeroUnitario(novoValor, 0);
+            }
+
+            return resultado;
+        }
+
+        private static int RetornaNumeroUnitario(string valor, int resultado)
+        {
+            for (int i = 0; i < valor.Length; i++)
+            {
+                resultado += int.Parse(valor.Substring(i, 1));
+
+            }
+
+
+
+            return resultado;
+        }
+    }
+}
---
+++ embaralhator2000/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace embaralhator2000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string again = string.Empty;
+
+            Console.WriteLine("############## EMBARALHATOR ##############");
+
+            do
+            {
+                EmbaralhatorMethod();
+
+                Console.WriteLine("##########################################");
+
+                Console.WriteLine("\nGostaria de executar novamente?  ");
+                again = Console.ReadLine();
+
+                while (!again.ToString().ToUpper().Equals("Y") && !again.ToString().ToUpper().Equals("N"))
+                {
+                    Console.WriteLine("\nSão aceitos apenas 'Y' para 'Sim' e 'N' para 'Não'.");
+                    Console.WriteLine("\nGostaria de executar novamente?  ");
+                    again = Console.ReadLine();
+                }
+
+            } while (!again.ToString().ToUpper().Equals("N"));
+
+
+            Console.WriteLine("\n\nOBRIGADO!\n");
+            Console.WriteLine("##########################################");
+            Console.ReadKey();
+
+        }
+
+        private static void EmbaralhatorMethod()
+        {
+            Console.WriteLine("Quantos registros deseja criar?  ");
+
+            int qtde;
+            int tentativas = 0;
+            while (!int.TryParse(Console.ReadLine(), out qtde))
+            {
+                tentativas++;
+                Console.WriteLine("\nEstúpido! São permitidos apenas números!");
+                Console.WriteLine();
+                Console.WriteLine("Quantos registros deseja criar?  ");
+
+            }
+
+            if (qtde > 0)
+            {
+                if (tentativas > 0)
+                    Console.WriteLine("\nAgora sim múmia paralítica!");
+
+                var inicio = DateTime.Now;
+
+                var content = BO.GeradorDeMassa(qtde);
+
+                var fim = DateTime.Now;
+
+                var diff = fim - inicio;
+
+                Console.WriteLine();
+                Console.WriteLine(string.Format("Retornou a letra {0}", content[0].Split(';')[1]));
+                Console.WriteLine();
+                Console.WriteLine("##########################################");
+                Console.WriteLine("Tempo total: " + diff);
+
+                DAO.PersistCSV(content);
+            }
+            else
+                Console.WriteLine("\n0....Você jura!?\n\nVai a merda, não me faça perder tempo!!!\nTCHAU!");
+        }
+    }
+}
